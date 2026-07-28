ae3147fc9a125653f532cc2a5b265311080519fa
joao pedro nascimento <pedronascimentofreittas@gmail.com>
2026-07-28 14:21:12 -0300

    adiciona pesquisa,categorias (atualização)

--- projeto/script.js
+++ projeto/script.js
@@ -1,3 +1,39 @@
+console.log(document);
+console.log(document.body)
+console.log(document.title)
+
+//const meuTitulo=document.querySelector("h1")
+//console.log(meuTitulo)
+
+//meuTitulo.textContent="testando com textContent";
+//xss
+//performace
+// textcontent sempre indicado 
+// createEçement.append.prePend.before.remove
+//append nao exclui os nodes que já axistem ele apenas adiciona em suguida 
+//preppend adiciona antes de todo mundo pré
+
+//const meuSpan = document.createElement("span");
+//meuSpan.textContent = "text-span"
+//meuSpan.id = "span-local"
+//console.log(meuSpan);
+//meuTitulo.append(meuSpan)
+//meuTitulo.prepend(meuSpan)
+
+//meuTitulo.prepend("novo conteudo")
+
+//meuTitulo.after("depois")// ADICIONA COMO IRMAO - fora do h1 literalmente depois/ adiciona fora do elemento que a gente adicionou
+//meuTitulo.before("antes")//ADICIONA COMO IRMAO - fora do h1 literalmente antes/ adiciona fora do elemento que a gente adicionou
+
+//meuSpan.after("NOVO CONTEUDO AAAAA")
+
+
+
+
+
+
+
+
 let carrinho = [];
 let favoritos = [];
 
@@ -18,35 +54,59 @@ function adicionarAosFavoritos(item) {
 }
 //atualizar ao carregar a página
 atualizarContadores();
-adicionarAoCarrinho("hot rolls");
-adicionarAoCarrinho("hot rolls");
-adicionarAoCarrinho("hot rolls");
-adicionarAosFavoritos("temaki");    
-adicionarAosFavoritos("temaki");    
-adicionarAosFavoritos("temaki");    
 
+const inputPesquisa = document.getElementById("pesquisa");
+const cards = document.querySelectorAll(".card");
 
+inputPesquisa.addEventListener("input", pesquisar);
 
-//carrinho
-//remover item do carrinho
+function pesquisar() {
+    const texto = inputPesquisa.value.toLowerCase();
 
-/** const botoesRemover =  document.querySelectorAll(".remover");
-botoesRemover.forEach(botao=>{
-    botao.addEventListener("click",()=>{
-    const itemCarrinho = botao.closest (".item-carrinho");
-    itemCarrinho.remove();
-});
-});
+    cards.forEach(card => {
+        const nome = card.querySelector("h3").textContent.toLowerCase();
 
-function atualizarCarrinho(){
-    const itens =document.querySelectorAll(".item-carrinho");
-    itens.forEach(item=>{
-        const botao=item.querySelector("remover");
-        botao.addEventListener("click",()=>{
-            item.remove();
-            //atualizar carrinho
-            atualizarCarrinho( );
-        })
-    })
+        if (nome.includes(texto)) {
+            card.style.display = "block";
+        } else {
+            card.style.display = "none";
+        }
+    });
 }
-atualizarCarrinho() */
+
+const botoesCategoria = document.querySelectorAll(".categoria-btn");
+
+botoesCategoria.forEach(botao => {
+    botao.addEventListener("click", filtrarCategoria);
+});
+
+function filtrarCategoria(evento) {
+
+    const categoria = evento.target.dataset.categoria;
+
+    cards.forEach(card => {
+
+      if (
+    categoria === "Todos" ||
+    card.dataset.categoria.split(",").includes(categoria)
+    
+) {
+    card.style.display = "";
+} else {
+    card.style.display = "none";
+}
+
+    });
+
+}
+
+
+
+
+
+
+
+
+
+
+
